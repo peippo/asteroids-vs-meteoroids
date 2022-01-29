faede9195631d7f9bc3b5eea0e3bcf6d94240c07
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-01-29 19:26:12 +0200

    Rotate camera on menu

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,12 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { StoreContext } from "../store";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { extend, useFrame, useThree } from "@react-three/fiber";
 
 extend({ OrbitControls });
 
 const Controls = () => {
+	const {
+		myId: [myId],
+		opponentId: [opponentId],
+		currentGameId: [currentGameId],
+	} = useContext(StoreContext);
 	const { camera, gl, invalidate } = useThree();
 	const ref = useRef();
+
+	const isInMenu = !myId || !opponentId || !currentGameId;
 
 	useFrame(() => ref.current.update());
 	useEffect(
@@ -16,10 +24,13 @@ const Controls = () => {
 
 	return (
 		<orbitControls
+			autoRotate={isInMenu}
+			enabled={!isInMenu}
 			ref={ref}
 			enableDamping
 			dampingFactor={0.03}
 			enableZoom={false}
+			enablePan={false}
 			args={[camera, gl.domElement]}
 		/>
 	);
