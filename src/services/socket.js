70b162a033c2f2094cbf0d564510534b6f1e8f62
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-01-30 13:56:27 +0200

    Add production server url

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,9 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io("http://127.0.0.1:4000");
+export const socket = io(
+	process.env.NODE_ENV === "production"
+		? "https://asteroids-vs-meteoroids.herokuapp.com/"
+		: "http://127.0.0.1:4000"
+);
 export const SocketContext = createContext();
