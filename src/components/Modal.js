69367b1b46cfd668c0fc014cbbf771fd8f196381
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-01-30 10:05:10 +0200

    Adjust modal & button styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,9 +13,30 @@ const Container = styled.div`
 	z-index: 1000;
 	background: var(--hud-background);
 	box-shadow: var(--hud-box-shadow), 0 20px 50px rgb(0 0 0 / 50%);
-	border-radius: 5px;
+	border-radius: 10px;
 	padding: 2rem 3rem;
 	text-align: center;
+
+	&:before,
+	&:after {
+		content: "";
+		position: absolute;
+		top: -10px;
+		width: 100px;
+		height: calc(100% + 20px);
+	}
+
+	&:before {
+		left: -10px;
+		border-left: 2px solid var(--color-green);
+		border-radius: 20px 0 0 20px;
+	}
+
+	&:after {
+		right: -10px;
+		border-right: 2px solid var(--color-green);
+		border-radius: 0 10px 10px 0;
+	}
 `;
 
 export default Modal;
