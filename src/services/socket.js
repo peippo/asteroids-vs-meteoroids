56a1bbc7d43d104d1291b4080211d1a7da6c0490
Janne Peippo <peippo@gmail.com>
2022-09-05 21:59:21 +0300

    Adjust socket server url

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 export const socket = io(
 	process.env.NODE_ENV === "production"
-		? "https://asteroids-vs-meteoroids.herokuapp.com/"
+		? "https://asteroids-vs-meteoroids-server.up.railway.app/"
 		: "http://127.0.0.1:4000"
 );
 export const SocketContext = createContext();
